25e0910a9066c3af5d855291f73016b81ee34ee0
logonrm <logonrm@fiap.com.br>
2018-02-24 14:09:31 -0300

    solucao completa

---
+++ UploadFotosFiap11NetService/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UploadFotosFiap11NetService.Models;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Azure;
+
+namespace UploadFotosFiap11NetService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var list = new List<ImageFile>();
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            
+            CloudBlobContainer container = blobClient.GetContainerReference("uploadfotos");
+            
+            foreach (IListBlobItem item in container.ListBlobs(null, false))
+            {
+                var imageFile = new ImageFile();
+
+                if (item.GetType() == typeof(CloudBlockBlob))
+                {
+                    CloudBlockBlob blob = (CloudBlockBlob)item;
+
+                    imageFile.Name = blob.Name;
+                    imageFile.Url = blob.Uri.ToString();
+
+                    list.Add(imageFile);
+                }
+            }
+            return View(list);
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase pic)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference("uploadfotos");
+            container.CreateIfNotExists();
+
+            container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(pic.FileName);
+
+            blockBlob.UploadFromStream(pic.InputStream);
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ UploadFotosFiap11NetService/Models/ImageFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UploadFotosFiap11NetService.Models
+{
+    public class ImageFile
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
